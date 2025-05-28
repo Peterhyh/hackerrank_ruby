af06061474b464826254bf31624812f11830670e
Peter Huynh <peterhyh@yahoo.com>
2025-05-27 19:12:08 -0500

    add block and yield

---
+++ block_and_yield.rb
@@ -0,0 +1,11 @@
+#block_message_printer prints the message if the block exists.
+def block_message_printer
+  message = "Welcome to Block Message Printer"
+  if block_given?
+      yield
+  end
+puts "But in this function/method message is :: #{message}"
+end
+
+message = gets
+block_message_printer { puts "This message remembers message :: #{message}" }

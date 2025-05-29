bc73361adf513285baaf4bb2d4d6502d0888a035
Peter Huynh <peterhyh@yahoo.com>
2025-05-28 20:07:38 -0500

    add lambda within lambda

--- lambda_within_lambda.rb
+++ lambda_within_lambda.rb
@@ -1,12 +1,13 @@
-combination = lambda{|n|
-	lambda{|r|
-		k=1
-		r.times{|i|k=k*(n-i)/(i+1)}
-		k
-	}
-}
+combination = -> (n) do
+  -> (r) do
+    k=1
+    r.times {|i| k=k*(n-i)/(i+1)}
+    k
+  end
+end
 
 n = 3
 r = 2
 nCr = combination.(n)
 puts nCr.(r)
+
